71f3d3c18b94013ef3a907a1a008021bdab2a2d1
GillesVandewiele <givdwiel.vandewiele@ugent.be>
2018-03-05 11:11:02 +0100

    set base url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'https://gillesvandewiele.github.io/KMBlog/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
